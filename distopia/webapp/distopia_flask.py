91f186f4ab8a60fa46300d72638546fe0ca2f76c
mattvlaw <mattvlaw@gmail.com>
2019-10-11 10:54:08 -0400

    added a skeleton eval function

--- distopia/webapp/distopia_flask.py
+++ distopia/webapp/distopia_flask.py
@@ -1,11 +1,25 @@
 from flask import Flask
+from flask import request
+from distopia.app.agent import VoronoiAgent
 import os
 app = Flask(__name__)
 
 @app.route("/")
 def root():
-    print(os.getcwd())
     return app.send_static_file("html/index.html")
+
+
+@app.route("/evaluate", methods=['POST'])
+def evaluate():
+    # only support POST
+    if request.method == 'POST':
+        # this is pseudo-code, it may (likeley will) not run without some tweaks
+        d_agent = VoronoiAgent()
+        d_agent.load_data()
+        blocks = request.json
+        districts = d_agent.get_voronoi_districts(blocks)
+        metrics = d_agent.compute_voronoi_metrics(districts)
+        return jsonify((data,metrics)) # this may choke as well
 
 if __name__ == "__main__":
     app.run()
