d2b0d8c07e4c603f61c4a8244ed91f5bff550b8a
Siddharth <siddhartha.srinivasan@gmail.com>
2019-10-17 16:51:11 -0400

    moved visual elements to elements file out of distopia interface

--- distopia/webapp/static/js/distopiaInterface.js
+++ distopia/webapp/static/js/distopiaInterface.js
@@ -7,189 +7,7 @@
 import {DistrictView} from "./districtView.js";
 import {StateView} from "./stateView.js";
 
-d3.json('http://localhost:5000/evaluate', {
-      method:"POST",
-      body: JSON.stringify({
-		  "blocks": {
-			0: [[263,678],[261,330]],
-			1: [[603,206],[708,188]],
-			2: [[765,385],[588,430],[488,530]],
-			3: [[473,185],[383,530],[375,640],[505,356]],
-			4: [[755,576],[838,371]],
-			5: [[733,113],[483,46]],
-			6: [[818,26]],
-			7: [[823,135]]
-		  },
-		  "packet_count": 0
-		
-      }),
-      headers: {
-        "Content-type": "application/json; charset=UTF-8"
-      }
-	})
-	.then((data) =>{
-		console.log(data);
-	})
-
-
 var MIN_X, MIN_Y, MAX_X, MAX_Y;
-
-//These are global color scales for different metrics
-//To invoke, scales.[NAME OF SCALE](VALUE) ex: scales.partisanFill(0.5)
-export var SCALE = {
-	//every scale, get scaleMax, scaleMin, scaleVale
-	"age": function([median_age,total_pop]){
-		let scale = d3.scaleLinear().domain([35,55]).range(["white","#C93FFF"]);
-		return scale(median_age);
-	},
-	"education": function([num_college, total_pop]){
-		//percentage with bachelor's degree or higher
-		let scale = d3.scaleLinear().domain([0, 1]).range(["white", "purple"]);
-		return scale(num_college/total_pop);
-	},
-	"income": function([median_income, tot_pop]){
-		let scale = d3.scaleLinear().domain([35000, 70000]).range(["white", "green"]);
-		return scale(median_income);
-	},
-	"occupation": function([num_employed, total_pop]){
-		//percentage employed out of total population
-		let scale = d3.scaleLinear().domain([0.45,0.55]).range(["white", "pink"]);
-		return scale(num_employed/total_pop);
-	},
-	"population": function([pop_voting, total_pop]){
-		//voting population out of 3 million (or max which will be defined later)
-		let scale = d3.scaleLinear().domain([0,3000000]).range(["white", "orange"]);
-		return scale(pop_voting);
-	},
-	"projected_votes": function([num_democrat, total_votes]){
-		//lean to either republican or democrat
-		// let scale = d3.scaleLinear().domain([-1, 0, 1]).range(["#D0021B","white", "#4A90E2"]);
-		// let prop_democrat = num_democrat/total_votes;
-		// let prop_republican = 1 - prop_democrat;
-		// return scale(prop_democrat - prop_republican);
-		let scale = d3.scaleLinear().domain([0, 0.5, 1]).range(["#D0021B","white", "#4A90E2"]);
-		let prop_democrat = num_democrat/total_votes;
-		return scale(prop_democrat);
-	},
-	"race": function([num_minorities, total_pop]){
-		//number nonwhite divided by total population
-		let scale = d3.scaleLinear().domain([0,1]).range(["white", "#102C42"]);
-		return scale(num_minorities/total_pop);
-	},
-	"pvi": function([wasted_votes,_]){
-		let scale = d3.scaleLinear().domain([0,200000]).range(["white","red"]);
-		return scale(wasted_votes)
-	},
-	"compactness": function([compactness,_]){
-		let scale = d3.scaleLinear().domain([0,1]).range(["white","green"]);
-		return scale(compactness)
-	}
-}
-
-export var DOMAIN = {
-	"age": {
-		domain: [35,55],
-		label: "years old"
-	},
-	"education": {
-		domain: [0, 1],
-		label: "College Educated"
-	},
-	"income": {
-		domain: [35000, 70000],
-		label: "annual income"
-	},
-	"occupation": {
-		domain: [0.45,0.55],
-		label: "employed"
-	},
-	"population": {
-		domain: [0,3000000],
-		label: "voters"
-	},
-	"projected_votes": {
-		domain: [0, 1],
-		label: "Democrat"
-	},
-	"race":{
-		domain: [0,1],
-		label: "Non-white"
-	},
-	"pvi":{
-		domain: [0,200000],
-		label: "Wasted Votes"
-	},
-	"compactness": {
-		domain: [0,1],
-		label: "Compactness"
-	}
-}
-
-export const METRICS = ["age","education","income","occupation","population","projected_votes","race","sex"]
-export const METRIC_TYPE = ["histogram","histogram","histogram","histogram","histogram","histogram","histogram","histogram"]
-export const STYLES = {
-	"race": {
-		colors:{
-			"white": "#E6AF81",
-			"Black": "#E68882",
-			"Hispanic": "#8A82E5",
-			"Asian": "#BDE682",
-			"Native American": "#82E0E6",
-			"pacific_islander": "#CCCCCC",
-			"Other": "#000000",
-			"Two or More": "#444444"
-		}
-	},
-	"population": {
-		colors:{
-			"Total Population": "#CCCCCC",
-			"Voting Population": "#82E0E6"
-		}
-	},
-	"age":{
-		colors: {
-			"0 to 10":"#447C1C",
-			"10 to 20":"#588B20",
-			"20 to 30":"#6E9A25",
-			"30 to 40":"#87A82A",
-			"40 to 50":"#A1B62F",
-			"50 to 60":"#BDC434",
-			"60 to 70":"#D2CA39",
-			"70 to 80":"#E0C63E",
-			"80+":"#EDC044",
-		}
-	},
-	"education":{
-		colors:{
-			"High School/GED": "#CCCCCC",
-			"Bachelor's": "#82E0E6"
-		}
-	},
-	"projected_votes":{
-		colors:{
-			"Democrat": "#4A90E2",
-			"Republican": "#D0021B"
-		}
-	},
-	"income":{
-		colors: {
-			"$0 to $25k": "#D7F6FF",
-			"$25k to $50k": "#B1D4FF",
-			"$50k to $75k" :"#8C9EFF",
-			"$75k to $100k" :"#7E6AFF",
-			"$100k +" :"#9449FF"
-		}
-	},
-	"occupation":{
-		colors:{
-			"Manufacturing": "#E6AF81",
-			"Retail": "#E68882",
-			"Professional": "#8A82E5",
-			"Public": "#BDE682",
-			"Service": "#82E0E6",
-		}
-	}
-}
 
 var SELF;
 
@@ -432,6 +250,31 @@ export const parseData = (labels, data) => {
 }
 
 export var distopia = new DistopiaInterface();
+d3.json('http://localhost:5000/evaluate', {
+      method:"POST",
+      body: JSON.stringify({
+		  "blocks": {
+			0: [[263,678],[261,330]],
+			1: [[603,206],[708,188]],
+			2: [[765,385],[588,430],[488,530]],
+			3: [[473,185],[383,530],[375,640],[505,356]],
+			4: [[755,576],[838,371]],
+			5: [[733,113],[483,46]],
+			6: [[818,26]],
+			7: [[823,135]]
+		  },
+		  "packet_count": 0
+		
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8"
+      }
+	})
+	.then((data) =>{
+		distopia.handleData({data: JSON.stringify(data)});
+		console.log(data);
+	})
+// export var distopia = new DistopiaInterface();
 
 $(".button").click(() =>{
 	distopia.toggleView();
--- distopia/webapp/static/js/stateView.js
+++ distopia/webapp/static/js/stateView.js
@@ -3,7 +3,8 @@
 	==========
 	A statewide view of a selected metric, with a heatmap and a set of histograms
 */
-import {parseData, METRICS, METRIC_TYPE, STYLES, SCALE, DOMAIN} from './distopiaInterface.js'
+import {parseData} from './distopiaInterface.js'
+import {DOMAIN, SCALE, METRICS, METRIC_TYPE, STYLES} from './distopiaElements.js'
 import Histogram from "./viz/histogram.js";
 
 var SELF;
