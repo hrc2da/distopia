bd8df4522c9f9edb1baee98b495c1c3987c01d2e
Siddharth <siddhartha.srinivasan@gmail.com>
2019-12-05 23:31:24 -0500

    trying to update active button

--- distopia/webapp/static/js/GUI.js
+++ distopia/webapp/static/js/GUI.js
@@ -91,6 +91,20 @@ export function updateState(newState) {
   State = { ...State, ...newState };
 }
 
+
+function updateAction(activeButton){
+  const buttons = [document.getElementById("metric_select_population"),document.getElementById("metric_select_voter_efficiency"),document.getElementById("metric_select_compactness")];
+  buttons.forEach(button => {
+    if (button == activeButton){
+      button.className = "active"
+    }
+    else {
+      button.className = "metric_select";
+    }
+  })
+
+}
+
 function initInteractive() {
   const centroidAddButton = document.getElementById("state");
   centroidAddButton.onclick = e => addCentroid(e);
@@ -99,14 +113,17 @@ const metric_select_population = document.getElementById("metric_select_populati
 console.log(metric_select_population);
 metric_select_population.onclick = () =>
   updateState({ metricFocus: "population" });
+  updateAction(metric_select_population);
 
 const metric_select_voter_efficiency = document.getElementById("metric_select_voter_efficiency");
 metric_select_voter_efficiency.onclick = () =>
   updateState({ metricFocus: "pvi"});
+  updateAction(metric_select_voter_efficiency);
 
 const metric_select_compactness = document.getElementById("metric_select_compactness");
 metric_select_compactness.onclick = () =>
 updateState({ metricFocus: "compactness"});
+updateAction(metric_select_compactness);
 
 
 
