5c677dde517193e0175a86759fa699713c55eacd
Siddharth <siddhartha.srinivasan@gmail.com>
2019-12-20 14:13:32 -0800

    added firebase scripts into index. set up  firebase.js script as well

---
+++ distopia/webapp/static/js/firebase.js
@@ -0,0 +1,6 @@
+import CONFIG from "~/Desktop/config.js"
+// Your web app's Firebase configuration
+// Initialize Firebase
+firebase.initializeApp(CONFIG);
+console.log('initialized firebase');
+firebase.analytics();
